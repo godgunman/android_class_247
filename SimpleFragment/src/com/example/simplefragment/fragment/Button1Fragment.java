d21d1b5cae031763b4395fd94c4a002513bae74b
ggm <godgunman@gmail.com>
2015-01-05 20:27:49 +0800

    Add OnClickListener

--- SimpleFragment/src/com/example/simplefragment/fragment/Button1Fragment.java
+++ SimpleFragment/src/com/example/simplefragment/fragment/Button1Fragment.java
@@ -4,20 +4,37 @@ import com.example.simplefragment.R;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Button1Fragment extends Fragment {
+
+	private Button button;
 
 	public Button1Fragment() {
 	}
 
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_button1, container, false);
-        return rootView;
-    }
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_button1, container,
+				false);
+		button = (Button) rootView.findViewById(R.id.button1);
+		button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.d("debug", "[Button1Fragment] buton1");
+				Toast.makeText(getActivity(), "[Button1Fragment] buton1",
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		return rootView;
+	}
 
 }
--- SimpleFragment/src/com/example/simplefragment/fragment/Button2Fragment.java
+++ SimpleFragment/src/com/example/simplefragment/fragment/Button2Fragment.java
@@ -2,14 +2,21 @@ package com.example.simplefragment.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.simplefragment.R;
 
 public class Button2Fragment extends Fragment {
 
+	private Button button1;
+	private Button button2;
+	
     public Button2Fragment() {
     }
 
@@ -17,6 +24,30 @@ public class Button2Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_button2, container, false);
+        button1 = (Button) rootView.findViewById(R.id.button1);
+        button2 = (Button) rootView.findViewById(R.id.button2);
+        
+        button1.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.d("debug", "[Button2Fragment] buton1");
+				Toast.makeText(getActivity(), "[Button2Fragment] buton1",
+						Toast.LENGTH_SHORT).show();
+	
+			}
+		});
+        
+        button2.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.d("debug", "[Button2Fragment] buton2");
+				Toast.makeText(getActivity(), "[Button2Fragment] buton2",
+						Toast.LENGTH_SHORT).show();
+				
+			}
+		});
+
+        
         return rootView;
     }
 
