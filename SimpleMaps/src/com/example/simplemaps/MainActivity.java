eca66ccbf6a4bdb2e964855e0a894b4b147eec67
godgunman <godgunman@gmail.com>
2015-01-08 21:01:22 +0800

    move camera and add marker listener

--- SimpleMaps/src/com/example/simplemaps/MainActivity.java
+++ SimpleMaps/src/com/example/simplemaps/MainActivity.java
@@ -1,33 +1,53 @@
 package com.example.simplemaps;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	
+
 	private MapFragment mapFragment;
 	private GoogleMap googleMap;
-	private final static LatLng NTU_POSITION  = new LatLng(25.017317, 121.539586);
+	private final static LatLng NTU_POSITION = new LatLng(25.017317, 121.539586);
+	private final static LatLng YANG_MING_SHAN = new LatLng(25.194367, 121.560957);
 
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+
+		mapFragment = (MapFragment) getFragmentManager().findFragmentById(
+				R.id.map);
 		googleMap = mapFragment.getMap();
-		
-		MarkerOptions markOption = new MarkerOptions().position(NTU_POSITION).title("NTU");
-		googleMap.addMarker(markOption);
+
+		MarkerOptions markerOption = new MarkerOptions().position(NTU_POSITION)
+				.title("NTU");
+		googleMap.addMarker(markerOption);
 		googleMap.setMyLocationEnabled(true);
+		googleMap.setOnMarkerClickListener(new OnMarkerClickListener() {
+			@Override
+			public boolean onMarkerClick(Marker marker) {
+				String title = marker.getTitle();
+				Toast.makeText(MainActivity.this, "title:" + title,
+						Toast.LENGTH_SHORT).show();
+				return false;
+			}
+		});
+	}
+
+	public void goToYangMingShan(View view) {
+		googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(YANG_MING_SHAN, 15));
 	}
 
 	@Override
