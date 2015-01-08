c4d25e67b992349551be1843e76cccd3c5b95a54
godgunman <godgunman@gmail.com>
2015-01-08 21:37:05 +0800

    add fetch url method

--- SimpleMaps/src/com/example/simplemaps/MainActivity.java
+++ SimpleMaps/src/com/example/simplemaps/MainActivity.java
@@ -1,5 +1,12 @@
 package com.example.simplemaps;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
@@ -10,6 +17,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,7 +28,8 @@ public class MainActivity extends Activity {
 	private MapFragment mapFragment;
 	private GoogleMap googleMap;
 	private final static LatLng NTU_POSITION = new LatLng(25.017317, 121.539586);
-	private final static LatLng YANG_MING_SHAN = new LatLng(25.194367, 121.560957);
+	private final static LatLng YANG_MING_SHAN = new LatLng(25.194367,
+			121.560957);
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +53,38 @@ public class MainActivity extends Activity {
 				return false;
 			}
 		});
+		
+		String result = fetch("https://api.foursquare.com/v2/venues/40a55d80f964a52020f31ee3");
+		Log.d("debug", result);
 	}
 
 	public void goToYangMingShan(View view) {
-		googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(YANG_MING_SHAN, 15));
+		googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(YANG_MING_SHAN,
+				15));
+	}
+
+	private String fetch(String urlString) {
+
+		try {
+			URL url = new URL(urlString);
+			URLConnection urlConnection = url.openConnection();
+
+			BufferedReader bufferedReader = new BufferedReader(
+					new InputStreamReader(urlConnection.getInputStream()));
+
+			String line;
+			StringBuilder builder = new StringBuilder();
+			while ((line = bufferedReader.readLine()) != null) {
+				builder.append(line);
+			}
+			return builder.toString();
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	@Override
