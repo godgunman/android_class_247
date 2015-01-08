9b6c45e8b26c67d79c7ddb3443dca5dd2050a1e5
godgunman <godgunman@gmail.com>
2015-01-08 20:39:01 +0800

    get googlemap

--- SimpleMaps/src/com/example/simplemaps/MainActivity.java
+++ SimpleMaps/src/com/example/simplemaps/MainActivity.java
@@ -1,16 +1,33 @@
 package com.example.simplemaps;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+	
+	private MapFragment mapFragment;
+	private GoogleMap googleMap;
+	private final static LatLng NTU_POSITION  = new LatLng(25.017317, 121.539586);
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+		googleMap = mapFragment.getMap();
+		
+		MarkerOptions markOption = new MarkerOptions().position(NTU_POSITION).title("NTU");
+		googleMap.addMarker(markOption);
+		googleMap.setMyLocationEnabled(true);
 	}
 
 	@Override
